12c20d2658b3335cd0798367e882471e00c10f5d
saicharan96 <saicharan14996@gmail.com>
2019-05-11 19:30:55 +0530

    dd
    ddd

--- cabpartner/index.php
+++ cabpartner/index.php
@@ -13,6 +13,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+    <body style="background-color:#eff3f6">
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="navbar-header">
@@ -122,10 +123,100 @@
    <textarea  class="controls textarea"  type="text" placeholder="Enter your Queries for us *" name="drop" ></textarea>
    </div>
    </div>
-   <center><button class="buttonexplore">Join with Minicabee</center>
-      </div>  
+            <center><button class="buttonexplore">Join with Minicabee</button></center>
+          </div>  </div></div>
     </section>
 <br><br>
-
+        <!--footer starts from here-->
+<footer class="footer hidden-sm hidden-xs">
+    <div class="container">
+<div class="row topping">
+      <div class="col-md-3 col-xs-6">
+        <p>Locations</p>
+    <ul>  
+            <li><a href="#">Heathrow Taxi</a></li>
+        <li><a href="#">Gatwick Taxi</a></li>
+        <li><a href="#">Birmingham Taxi</a></li>
+        <li><a href="#">Liverpool Taxi</a></li>
+        <li><a href="#">Manchester Taxi</a></li>
+        <li><a href="#">London Taxi</a></li>
+        <li><a href="#">Cardiff Taxi</a></li>
+        <li><a href="#">Norwich Taxi</a></li>
+        <li><a href="#">Lincoln Taxi</a></li>
+        <li><a href="#">Plymouth Taxi</a></li>
+        <li><a href="#">Guildford Taxi</a></li>
+        <li><a href="#">Doncaster Taxi</a></li>
+        <li><a hrf="">Leicestershire Taxi</a></li>
+        <li><a hrf="">Aberdeen Taxi</a></li>
+        <li><a hrf="">Sheffield Taxi</a></li>
+        <li><a hrf="">Oxford Taxi</a></li>
+    </ul>
+    </div>
+     <div class="col-md-3 col-xs-6">
+        <p>AIRPORTS</p>
+    <ul>  
+            <li><a href="#">Heathrow Airport Taxi</a></li>
+        <li><a href="#">Gatwick Airport Taxi</a></li>
+        <li><a href="#">Birmingham Airport Taxi</a></li>
+        <li><a href="#">London Airport Taxi</a></li>
+        <li><a href="#">Stansted Airport Taxi</a></li>
+        <li><a href="#">Luton Airport Taxi</a></li>
+        <li><a href="#">London City Airport Taxi</a></li>
+        <li><a href="#">Manchester Airport Taxi</a></li>
+        <li><a href="#">Edinburgh Airport Taxi</a></li>
+        <li><a href="#">Glasgow Airport Taxi</a></li>
+        <li><a href="#">Birmingham Airport Taxi</a></li>
+        <li><a href="#">Liverpool Airport Taxi</a></li>
+    </ul>
+    </div>
+     <div class="col-md-3 col-xs-6">
+        <p>TRAIN STATIONS</p>
+    <ul>  
+        <li><a href="#">Birmingham New Street Station Taxi</a></li>
+        <li><a href="#">Euston Station Taxi</a></li>
+        <li><a href="#">Manchester Piccadilly Station Taxi</a></li>
+        <li><a href="#">Paddington Station Taxi</a></li>
+        <li><a href="#">Stratford Station Taxi</a></li>
+        <li><a href="#">Charing Cross Station Taxi</a></li>
+        <li><a href="#">Reading Station Taxi</a></li>
+        <li><a href="#">Victoria Station Taxi</a></li>
+        <li><a href="#">Kings Cross Station Taxi</a></li>
+    </ul>
+    </div>
+      <div class="col-md-3 col-xs-6">
+        <p>COMPANY</p>
+    <ul>  
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Locations</a></li>
+        <li><a href="#">Discounts</a></li>
+        <li><a href="#">Book a Ride</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms & Conditions</a></li>
+    </ul>
+    </div>
+    </div>
+    </div>
+</footer>
+     <div class="darkfooter">
+         <div class="container">
+             <div class="row">
+                  <div class="col-md-6">
+                   <p>© Copyrights 2019 Minicabee. All rights reserved.</p>
+                 </div>
+                 <div class="col-md-6 hidden-sm hidden-xs">
+             <ul>
+                 <li>Sitemap</li>
+                <li>Contact</li>
+                 <li>Help</li>
+                 <li>Terms & Conditions</li>
+             </ul>
+                 </div>
+                
+             </div>
+             
+             </div>
+    </div>
+ <!--footer starts from here-->
+    </body>
 
 
